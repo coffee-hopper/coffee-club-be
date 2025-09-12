3a7783b4be3b2f6408b423789c525a8676b27d09
Bhdrpkcn <bhdrpkcn@gmail.com>
2025-09-12 14:01:53 +0300

    before notification flow

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -20,7 +20,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
         secret: configService.get<string>('JWT_SECRET'),
-        signOptions: { expiresIn: '1m' },
+        signOptions: { expiresIn: '1440m' },
       }),
       inject: [ConfigService],
     }),
